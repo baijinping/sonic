ee8d868013deb6b10a2753dfdf8ca8345fe62790
duanyi.aster <duanyi.aster@bytedance.com>
2023-05-08 10:52:37 +0800

    fmt

--- loader/pcdata.go
+++ loader/pcdata.go
@@ -1,11 +1,3 @@
-/*
- * @Author: duanyi.aster duanyi.aster@bytedance.com
- * @Date: 2023-04-19 11:46:53
- * @LastEditors: duanyi.aster duanyi.aster@bytedance.com
- * @LastEditTime: 2023-04-19 18:16:18
- * @FilePath: /sonic/loader/pcdata.go
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 /**
  * Copyright 2023 ByteDance Inc.
  *
@@ -25,7 +17,7 @@
 package loader
 
 import (
-	"encoding/binary"
+    `encoding/binary`
 )
 
 const (
